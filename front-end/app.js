881896b2260fae3cea969754a47ccef31fce414c
Edvinas <1181982@isep.ipp.pt>
2019-10-22 22:25:15 +0300

    Fixed shadow paramter now geting to table todo: add input for js function getting date parameter

--- front-end/app.js
+++ front-end/app.js
@@ -1,28 +1,36 @@
-fetch('http://localhost:4567/getJSON/2018-10-22',{
-    method: "GET",
-    headers: {
-        "Accept": "application/json"
-    }
-}).then(function (response) {
-    response.json().then((data) => {
-       data.items.item.forEach((item , index) => {
-           var table = document.getElementById("tableCurrency");
-           if(index === 0){
-               var tableRow = table.insertRow(index);
-               _.mapObject(item , function (val , key){
-                   var row = tableRow.insertCell(-1);
-                   row.outerHTML = "<th>"+val+"</th>";
-               });
-           }else {
-               var tableRow = table.insertRow(index);
-               _.mapObject(item , function (val , key){
-                   var row = tableRow.insertCell(-1);
-                   row.innerHTML = val;
-               });
-           }
-       });
+function createTable( date , tableID) {
+    console.log(tableID);
+    fetch('http://localhost:4567/getJSON/'+ date,{
+        method: "GET",
+        headers: {
+            "Accept": "application/json"
+        }
+    }).then(function (response ) {
+        console.log(this);
+        response.json().then((data) => {
+            data.items.item.forEach((item , index) => {
+                var table = document.getElementById(("tableCurrency"+ tableID ));
+                if(index === 0){
+                    var tableRow = table.insertRow(index);
+                    _.mapObject(item , function (val , key){
+                        var row = tableRow.insertCell(-1);
+                        row.outerHTML = "<th>"+val+"</th>";
+                    });
+                }else {
+                    var tableRow = table.insertRow(index);
+                    _.mapObject(item , function (val , key){
+                        var row = tableRow.insertCell(-1);
+                        row.innerHTML = val;
+                    });
+                }
+            });
+        });
+
+    }).catch(function (reason) {
+        //error
     });
 
-}).catch(function (reason) {
-    //error
-});
+}
+
+createTable("2019-09-09" , "1");
+createTable("2019-09-10" , "2");
