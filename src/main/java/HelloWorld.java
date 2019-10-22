9e51756bffd9ee769bcbddade377ffec19c7524b
Edvinas <1181982@isep.ipp.pt>
2019-10-22 18:14:56 +0300

    Managed to get xml information ant turn it on json

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,34 +1,43 @@
 
 
+import org.json.JSONException;
 import org.xml.sax.SAXException;
-
-
-
 import javax.xml.parsers.ParserConfigurationException;
 
 import javax.xml.transform.TransformerException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 
-import java.nio.charset.StandardCharsets;
+import java.net.URLConnection;
+import java.util.stream.Collectors;
 
+import org.json.JSONObject;
+import org.json.XML;
 import static spark.Spark.*;
 public class HelloWorld {
+    public static int PRETTY_PRINT_INDENT_FACTOR = 4;
+    public static String TEST_XML_STRING =
+            "<?xml version=\"1.0\" ?><test attrib=\"moretest\">Turn this to JSON</test>";
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException, TransformerException {
-        //BasicConfigurator.configure();
-        //URLConnection con = new URL("https://www.lb.lt/lt/currency/daylyexport/?xml=1&class=Eu&type=day&date_day=2019-10-16").openConnection();
-        //System.out.println(con);
-        //get("/hello", (req, res) -> "Hello World " + req.queryParams("foo"));
-
         String urlString = "";
         URL u = new URL("http://www.lb.lt/lt/currency/daylyexport/?xml=1&class=Eu&type=day&date_day=2019-10-16");
-        try (InputStream in = u.openStream()) {
-             String a = new String(in.readAllBytes(), StandardCharsets.UTF_8);
-            System.out.println(a);
+        URLConnection connection = u.openConnection();
+        String redirect = connection.getHeaderField("Location");
+        if (redirect != null){
+            connection = new URL(redirect).openConnection();
         }
-
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine = in.lines().collect(Collectors.joining());
+        try {
+            JSONObject xmlJSONObj = XML.toJSONObject(inputLine);
+            System.out.println(xmlJSONObj);
+        } catch (JSONException je) {
+            System.out.println(je.toString());
+        }
 
 // that’s the default xform; use a stylesheet to get a real one
 
